26b079e8366ab836d5636b30178a3f6feb1c0267
perlancar (@pc-home) <perlancar@gmail.com>
2015-08-17 20:25:58 +0700

    Switch from Log::Any to Log::Any::IfLOG

--- lib/File/Move/Undoable.pm
+++ lib/File/Move/Undoable.pm
@@ -6,7 +6,7 @@ package File::Move::Undoable;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any '$log';
+use Log::Any::IfLOG '$log';
 
 use Builtin::Logged qw(system);
 use File::MoreUtil qw(file_exists l_abs_path);
